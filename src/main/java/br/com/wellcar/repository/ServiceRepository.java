c161024cfa7b483cf20f8f5ac169627754a536d2
Elisberto Junior <elisberto_jr@hotmail.com>
2023-09-08 11:25:17 -0300

    feat: Added Service Repository

---
+++ src/main/java/br/com/wellcar/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package br.com.wellcar.repository;
+
+import br.com.wellcar.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
