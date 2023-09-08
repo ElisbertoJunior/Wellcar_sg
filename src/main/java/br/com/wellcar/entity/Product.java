6ca5dc00949cd9fd9bd1f7ffb4cbafce9fae18fe
Elisberto Junior <elisberto_jr@hotmail.com>
2023-09-08 11:21:53 -0300

    feat: Added Product entity

---
+++ src/main/java/br/com/wellcar/entity/Product.java
@@ -0,0 +1,24 @@
+package br.com.wellcar.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+}
