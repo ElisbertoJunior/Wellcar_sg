54d76903bc29859deb09196200e59b8a6c2ca371
Elisberto Junior <elisberto_jr@hotmail.com>
2023-09-15 16:32:10 -0300

    feat: Added SecurityConfiguration

---
+++ src/main/java/br/com/wellcar/security/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package br.com.wellcar.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "auth/register").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "report/create").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "report/period").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "report/is-open").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "report/all-reports").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "report/delete/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "report/{id}").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/br/com/wellcar/security/TokenService.java
@@ -0,0 +1,52 @@
+package br.com.wellcar.security;
+
+import br.com.wellcar.entity.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("wellcar")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+            return token;
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar o token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("wellcar")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
